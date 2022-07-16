04338b6c8a47331e97550d7f7cb3f37b024daa49
bishoDev123 <bisholiveenglish@gmail.com>
2022-07-16 23:28:53 +0200

    .

--- GMTK/Assets/Scripts/Enemy/EnemyController.cs
+++ GMTK/Assets/Scripts/Enemy/EnemyController.cs
@@ -15,9 +15,14 @@ public class EnemyController : MonoBehaviour
     public float health;
     public float maxHealth;
 
+    //tells whether this enemy is flying back from knockback
+    private bool knocking;
+
     // Start is called before the first frame update
     void Start()
     {
+        knocking = false;
+
         health = maxHealth;
         rb = GetComponent<Rigidbody2D>();
     }
@@ -27,25 +32,59 @@ public class EnemyController : MonoBehaviour
     {
         //gets the direction the player is from the enemy
         direction = player.position - transform.position;
+
+        //only set the exact velocity when not experiencing a knockback
+        if (!knocking)
+        {
+            //moves the enemy based on the direction
+            rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
         
-        //moves the enemy based on the direction
-        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        
+
 
         HPdisplay.SetHealth(health, maxHealth);
 
 
     }
 
+
+    private void FixedUpdate()
+    {
+        //this is needed for the knockback so that the knockback slows down
+        if (rb.velocity.magnitude > 0.0f && knocking)
+        {
+            rb.velocity *= 0.9f;
+            if (rb.velocity.magnitude < .5f)
+            {
+                knocking = false;
+                rb.velocity = Vector2.zero;
+            }
+        }
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "weapon")
         {
+            knockBackSelf();
+            
             health -= 1;
             if(health <= 0)
             {
                 Destroy(gameObject);
             }
         }
+    }
+
+    void knockBackSelf()
+    {
+        knocking = true;
+        //gets the direction vector from the player to this enemy
+        Vector3 dir = (transform.position - PlayerMovement.PM.transform.position).normalized;
+        //adds a velocity vector in that direction multiplied by the weapon power
+        rb.velocity += new Vector2(dir.x, dir.y)  * GameManager.GM.diceUp * 5.0f;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
